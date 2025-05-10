acbb61b54c04946b3b9d0acee47482fd388c5ba8
huyhoan286 <dohuyhoanhp@gmai.com>
2025-05-10 08:54:48 +0700

    Sửa finalize và áp dụng composition đúng cho lớp MayTinh

--- 21_2251172349_DoHuyHoan_BaiTap1/21_2251172349_DoHuyHoan_BaiTap1/src/hethong/Main.java
+++ 21_2251172349_DoHuyHoan_BaiTap1/21_2251172349_DoHuyHoan_BaiTap1/src/hethong/Main.java
@@ -2,9 +2,7 @@ package hethong;
 
 public class Main {
     public static void main(String[] args) {
-        BoNho boNho1 = new BoNho("DDR4", 16);
-        MayTinh mayTinh1 = new MayTinh("HP EliteBook", boNho1);
-
+        MayTinh mayTinh1 = new MayTinh("HP EliteBook", "DDR4", 16);
         mayTinh1.hienThiThongTin();
 
         // Kiểm tra xóa
--- 21_2251172349_DoHuyHoan_BaiTap1/21_2251172349_DoHuyHoan_BaiTap1/src/hethong/MayTinh.java
+++ 21_2251172349_DoHuyHoan_BaiTap1/21_2251172349_DoHuyHoan_BaiTap1/src/hethong/MayTinh.java
@@ -4,20 +4,14 @@ public class MayTinh {
     private String ten;
     private BoNho boNho;
 
-    public MayTinh(String ten, BoNho boNho) {
+    // Composition: Tạo BoNho bên trong MayTinh
+    public MayTinh(String ten, String loaiBoNho, int dungLuongBoNho) {
         this.ten = ten;
-        this.boNho = boNho;
+        this.boNho = new BoNho(loaiBoNho, dungLuongBoNho);
     }
 
     public void hienThiThongTin() {
         System.out.println("Tên máy tính: " + ten);
-        System.out.println("Thông tin bộ nhớ:\n" +  boNho.toString());
-    }
-
-    @Override
-    protected void finalize() throws Throwable {
-        System.out.println("Máy tính \"" + ten + "\" bị xóa. Bộ nhớ cũng sẽ được xóa.");
-        boNho = null; // Dọn dẹp
-        super.finalize();
+        System.out.println("Thông tin bộ nhớ:\n" + boNho.toString());
     }
 }
